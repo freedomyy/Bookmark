7bb67827d7df3d550681c4bea1a263aa09d7b356
freedomyy <freedomyy27@gmail.com>
2017-04-20 23:26:15 -0700

    finish

--- bookmarks/app/models/user.rb
+++ bookmarks/app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  has_many :bookmarks
+  belongs_to :user
   devise :database_authenticatable, :registerable,
          :rememberable, :validatable
 end
--- bookmarks/db/schema.rb
+++ bookmarks/db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170420232926) do
+ActiveRecord::Schema.define(version: 20170421000128) do
 
   create_table "bookmarks", force: :cascade do |t|
     t.string   "title"
     t.string   "url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
